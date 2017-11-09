53602dc8941442d2e67ea94a9ac3f7d638b57f7d
TreezyMaNeezy <33495354+TreezyMaNeezy@users.noreply.github.com>
2017-11-09 16:41:23 +0000

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -34,30 +34,3 @@ async def on_ready():
 
 if __name__ == "__main__" :
     bot.run("Mzc3ODc4ODMzOTI5NjUwMTc2.DOTWmg.1Sip3uihH1F-OG5DfXpraAx7PxI") # Put bot token here
-    
-class Xp:
-	async def defaultrole(self, ctx):
-		"""Lists the default role that new users are assigned."""
-		# Check if we're suppressing @here and @everyone mentions
-		if self.settings.getServerStat(ctx.message.guild, "SuppressMentions").lower() == "yes":
-			suppress = True
-		else:
-			suppress = False
-
-		role = self.settings.getServerStat(ctx.message.guild, "DefaultRole")
-		if role == None or role == "":
-			msg = 'New users are not assigned a role on joining this server.'
-			await ctx.channel.send(msg)
-		else:
-			# Role is set - let's get its name
-			found = False
-			for arole in ctx.message.guild.roles:
-				if str(arole.id) == str(role):
-					found = True
-					msg = 'New users will be assigned to **{Unassigned}**.'.format(arole.name)
-					# Check for suppress
-					if suppress:
-						msg = Nullify.clean(msg)
-			if not found:
-				msg = 'There is no role that matches id: `{}` - consider updating this setting.'.format(role)
-			await ctx.message.channel.send(msg)
